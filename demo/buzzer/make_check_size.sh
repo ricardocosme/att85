5cbd6f1d3873132bcc653d2e5d6d7f306ed66cb6
Ricardo Cosme <ricardo.cosme@me.com>
2020-01-19 19:35:08 -0300

    Excludes the draft related to the buzzer

--- demo/buzzer/make_check_size.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make $@
-avr-size beep.elf
-avr-size beep_pause_beep.elf
-avr-size beep_pause_beep_noclock.elf
-avr-size beep_pause_beep_custom_hilosig.elf
-avr-size beep_pause_beep_custom_hilosig_noclock.elf
--- include/att85/new.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stddef.h>
-
-inline void* operator new(size_t, void* p) noexcept
-{ return p; }

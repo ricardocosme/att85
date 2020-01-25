06bb16ff4525d9d959763149e63b43db4689f105
Ricardo Cosme <ricardo.cosme@me.com>
2020-01-25 02:00:54 -0300

    Add license to USI.hpp

--- include/att85/ssd1306/detail/USI.hpp
+++ include/att85/ssd1306/detail/USI.hpp
@@ -1,3 +1,9 @@
+
+// Copyright Ricardo Calheiros de Miranda Cosme 2020.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <att85/pin.hpp>
